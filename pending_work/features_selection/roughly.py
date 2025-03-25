bab4b2ea3f7912c481d87897655807cd0e979f50
gspeter-max <gspeter67@gmail.com>
2025-03-25 06:49:52 +0000

    Final cleanup and push

--- pending_work/features_selection/roughly.py
+++
@@ -1,5 +0,0 @@
-encoder = target_encoder(strategy = 'auto',data = df_pandas ,
-                         categorical_featues = ['cat_feature_0', 'cat_feature_1',
-       'cat_feature_2', 'cat_feature_3', 'cat_feature_4', 'cat_feature_5',
-       'cat_feature_6', 'cat_feature_7', 'cat_feature_8', 'cat_feature_9'] , 
-       target_feature = 'target' ) 
