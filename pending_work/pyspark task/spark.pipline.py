1a3d83f7752bf456c62f3543fc59bb3e1cd204a0
gspeter-max <gspeter67@gmail.com>
2025-03-26 08:31:28 +0000

    update

--- pending_work/pyspark task/spark.pipline.py
+++ pending_work/pyspark task/spark.pipline.py
@@ -2,13 +2,13 @@ from pyspark.sql import  SparkSession
 
 spark = SparkSession.builder.appName('pipeline').getOrCreate() 
 
-df_csv = spark.read.csv('/workspaces/machine_learning-projects-/pending_work/pyspark task/small_media_data.csv',
-                        header = True, inferSchema = True)
+# df_csv = spark.read.csv('/workspaces/machine_learning-projects-/pending_work/pyspark task/small_media_data.csv',
+#                         header = True, inferSchema = True)
 
-num_partitions = df_csv.rdd.getNumPartitions()  
+# num_partitions = df_csv.rdd.getNumPartitions()  
 
-df_csv = df_csv.repartition(num_partitions + 10)
-df_csv.write.option('compression','snappy').mode('overwrite').parquet('/workspaces/machine_learning-projects-/pending_work/pyspark task/small_media_data.parquet') 
+# df_csv = df_csv.repartition(8)
+# df_csv.write.option('compression','snappy').mode('overwrite').parquet('/workspaces/machine_learning-projects-/pending_work/pyspark task/small_media_data.parquet') 
 
 df_parquet = spark.read.parquet('/workspaces/machine_learning-projects-/pending_work/pyspark task/small_media_data.parquet')
 
@@ -41,12 +41,12 @@ df_parquet = spark.sql(
 from pyspark.sql.functions import when,cast , col , date_format,cast 
 from pyspark.sql.types  import DateType  
 
-df_parquet = df_parquet.withColumn('timestamp' , col('timestamp').cast(DateType())).withColumnRenamed('timestamp','date')
-df_parquet = df_parquet.withColumn('months' , date_format('date', 'M').cast('int'))
-df_parquet= df_parquet.withColumn('engagement_score', when(col('likes') > 500 ,1).otherwise(0))
-df_parquet = df_parquet.withColumn('media_type',when(col('media_type') == 'image', 1).when(col('media_type') == 'video',2).otherwise(3)).withColumnRenamed('media_type', 'media_type_encoded')
-df_parquet.show()
-
+df_parquet = df_parquet.selectExpr(
+    "cast(timestamp as date) as date", 
+    "cast(date_format(timestamp, 'M') as int) as month", 
+    "case when likes > 500 then 1 else 0 end as engagement_score", 
+    "case when media_type = 'image' then 1 when media_type = 'text' then 2 else 3 end as media_type_encoded" 
+)
 
 train , test = df_parquet.randomSplit([0.8,0.2], seed= 42)
 
@@ -65,13 +65,20 @@ vector = VectorAssembler(
 train_transformed = vector.transform(train)
 test_transformed = vector.transform(test)
 
-model = RandomForestClassifier(featuresCol='vector_list', labelCol='engagement_score') 
+model = RandomForestClassifier(
+    featuresCol = 'vector_list',
+    labelCol = 'engagement_score', 
+    numTrees= 50, 
+    maxDepth = 10, 
+    seed= 42 
+) 
+train_transformed.cache() 
+
 model = model.fit(train_transformed) 
 
 from pyspark.ml.evaluation import  BinaryClassificationEvaluator
 
-prediction = model.transform(test_transformed)
-prediction.show()
+prediction = model.transform(test_transformed).select('engagement_score','rawPrediction') 
 evaluator = BinaryClassificationEvaluator(
     labelCol = 'engagement_score',
     rawPredictionCol = 'rawPrediction', 
